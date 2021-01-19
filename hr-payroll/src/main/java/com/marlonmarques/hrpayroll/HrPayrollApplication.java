71b8dc34b8537ff03063b4b1ed0c55823f059ab6
Marlon <marlon.belohd@gmail.com>
2021-01-19 12:03:24 -0300

    project hr-payroll created

---
+++ hr-payroll/src/main/java/com/marlonmarques/hrpayroll/HrPayrollApplication.java
@@ -0,0 +1,13 @@
+package com.marlonmarques.hrpayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrPayrollApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrPayrollApplication.class, args);
+    }
+
+}
