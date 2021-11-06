4095c1dd91bc2475aca76ea08d771c1495ebb6bc
Marlon Marques <marlon.belohd@gmail.com>
2021-11-06 10:59:19 -0400

    login, jwt token

--- hr-oauth/src/main/java/com/marlonmarques/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/marlonmarques/hroauth/services/UserService.java
@@ -5,10 +5,13 @@ import com.marlonmarques.hroauth.feignclients.UserFeignClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
@@ -25,5 +28,14 @@ public class UserService {
 
         logger.info("Email found: " + email);
         return user;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        try {
+            return findByEmail(username);
+        } catch (IllegalArgumentException e) {
+            throw new UsernameNotFoundException("Email not found");
+        }
     }
 }
